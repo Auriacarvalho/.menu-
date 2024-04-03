52a078d6e118c5649247bbc2e3c638d9e56d46ed
Auriacarvalho <165321798+Auriacarvalho@users.noreply.github.com>
2024-04-02 21:34:19 -0300

    modelo para representar um item no catálogo de Animais
    Esse e um exemplo  em Java se  os colegas quiserem alterar alguma coisa, fique a vontade e tiver algo a corrigir também.

--- teste.java
+++ teste.java
@@ -1,16 +1,14 @@
-Import java.util.Scanner;
-public catalogue program{
-    Scanner console =new Scanner(System.in)
-ItemDelMEnu[]principal=new ItemDelMEnu
-new InserirAnimal(),
-new ListarAnimal(),
-new SAida()
-};
-do{
-for(in i= 0;i) < principal.length; i++){
-System.out.println(i + " - " + principal[i].getDescricao());
+Catalogo.main()
+public class ItemCatalogo {
+    private String nome;
+    private String descricao;
+    private String fotoPath; // Caminho da foto
+
+    public ItemCatalogo(String nome, String descricao, String fotoPath) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.fotoPath = fotoPath;
+    }
+
+    // getters e setters
 }
-System.out.print("Qual opção desejada");
-int opção = Integer.parserInt(console.nexline());
-principal[opção].executar();
-} while (!sair);
